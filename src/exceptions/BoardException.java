a3e8722b35436ea8f0ae20b3d0b27f85628a4a36
XxJoaoQueirozxX <joavic.pqueiroz@gmail.com>
2020-07-03 16:09:03 -0300

    -add BoardException -add ChessException -add Color enum

---
+++ src/exceptions/BoardException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class BoardException extends Exception{
+    public BoardException(String message) throws Exception {
+        super(message);
+    }
+}
---
+++ src/exceptions/ChessException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class ChessException extends Exception{
+    public ChessException(String message) {
+        super(message);
+    }
+}
